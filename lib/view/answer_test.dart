75d1e57c0b33f9d3fa500d2d409ae11913a6cd72
kouno <hamuichi9231@gmail.com>
2024-06-14 00:00:40 +0900

    解答カードのレイアウト作成

--- lib/view/answer_test.dart
+++ lib/view/answer_test.dart
@@ -3,6 +3,27 @@ import 'package:flutter/material.dart';
 import 'package:infinity_won/router.dart' as rt;
 import 'package:infinity_won/view_model/answer_test_view_model.dart';
 
+class AnswerButtonFlut extends ConsumerWidget{
+  const AnswerButtonFlut({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return TextButton(
+        style: TextButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: const BorderRadius.all(Radius.circular(8)),
+            side: BorderSide(
+              width: 2,
+            ),
+          ),
+        ),
+        onPressed: () {},
+        child: const Text('TextButton #3'),
+      );
+  }
+  
+}
+
 /// 解答画面
 class AnswerTest extends ConsumerStatefulWidget {
   const AnswerTest({super.key});
@@ -28,16 +49,19 @@ class _AnswerTestState extends ConsumerState<AnswerTest> {
         child: Column(
           children: [
             //TODO:高さを調整できない！けどデザイン考えてない！
-            Expanded(
+            SizedBox(
+              height: 300,
               child: ListView.builder(
                 scrollDirection: Axis.horizontal,
                 itemCount: 5,
                 itemBuilder: (BuildContext context, int index) {
                   return Card(
                     child: SizedBox(
-                      width: 200,
-                      height: 50,
-                      child: Center(child: Text("card")),
+                      width: MediaQuery.of(context).size.width - 20,
+                      child: Column(children: [
+                        Text("第$index問"),
+                         AnswerButtonFlut(),
+                      ],),
                     ),
                   );
                 },
@@ -50,3 +74,4 @@ class _AnswerTestState extends ConsumerState<AnswerTest> {
     );
   }
 }
+
